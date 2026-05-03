350e0de833afad2925bae950cec88de3f7cee250
PSPGuy <lootninja87@gmail.com>
2026-05-03 21:01:30 +0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,5 +1,5 @@
 /* TuxOS - a minimal operating system
- * Copyright (C) 2025  Your Name
+ * Copyright (C) 2025  PSPGuyCVM
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
